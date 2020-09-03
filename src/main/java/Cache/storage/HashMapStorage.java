74cb0e42549498075a0fa8d6206cb29532024ed1
tannikaM <tannika10@gmail.com>
2020-09-03 09:23:48 +0530

    Added implementation of hashmap based storage

--- src/main/java/Cache/storage/HashMapStorage.java
+++ src/main/java/Cache/storage/HashMapStorage.java
@@ -1,15 +1,41 @@
 package Cache.storage;
 
-public class HashMapStorage<Key, Value> implements Storage{
-    public void put(Object o, Object o2) {
+import java.util.HashMap;
+import Exception.*;
 
+public class HashMapStorage<Key, Value> implements Storage<Key, Value>{
+    private Integer capacity;
+    private HashMap<Key, Value> storage;
+
+    public HashMapStorage(Integer capacity){
+        HashMap<Key, Value> storage = new HashMap<Key, Value>();
+        this.storage = storage;
+        this.capacity = capacity;
     }
 
-    public Object get(Object o) {
-        return null;
+    public void put(Key key, Value value) throws StorageFullException{
+        try {
+            this.storage.put(key, value);
+            this.capacity = this.capacity + 1;
+        }catch (Exception ex){
+            throw new StorageFullException("Capacity of storage is full");
+        }
+    }
+
+    public Value get(Key key) throws KeyNotFound {
+        try {
+            Value value = this.storage.get(key);
+            return value;
+        }catch (Exception ex) {
+            throw new KeyNotFound("The key is not present");
+        }
     }
 
     public boolean isFull() {
-        return false;
+        return this.storage.size() == this.capacity;
+    }
+
+    public void removeKey(Key key) {
+        this.storage.remove(key);
     }
 }
--- src/main/java/Cache/storage/Storage.java
+++ src/main/java/Cache/storage/Storage.java
@@ -1,9 +1,11 @@
 package Cache.storage;
 
+import Exception.*;
+
 public interface Storage<Key, Value> {
 
-    public void put(Key key, Value value);
-    public Value get(Key key);
+    public void put(Key key, Value value) throws StorageFullException;
+    public Value get(Key key) throws KeyNotFound;
     public boolean isFull();
     public void removeKey(Key key);
 
